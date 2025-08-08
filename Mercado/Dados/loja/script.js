e63dd67497b20b18ac3d440b8ac8c2efe1d79f01
enzogamer123 <badfuture19@gmail.com>
2025-08-08 09:43:48 -0300

    Add files via upload

---
+++ Mercado/Dados/loja/script.js
@@ -0,0 +1,30 @@
+let produtos
+
+window.onload = function(){
+    var storeUser = localStorage.getItem("usuario")
+    var user = JSON.parse(storeUser)
+
+    var dataEntrada = new Date(user.dataEntrada);
+    var dataFormatada = dataEntrada.toDateString("pt-BR", {
+        day:"2-digit",
+        month:"2-digit",
+        year:"nuneric",
+        hour:"2-digit",
+        minute:"2-digit",
+    })
+
+    document.getElementById("perfil").innerText = dataFormatada
+    document.getElementById("user").innerText = user.name
+    document.getElementById("idPerfil").innerText = user.id
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch('../Dados/mock.json')
+        .then((response) =>response.json())
+        .then((data) =>{
+            produtos = data
+            console.log(data)
+        
+    }).catch((error) => console.error('Erro ao carregar dados', error))
+})
